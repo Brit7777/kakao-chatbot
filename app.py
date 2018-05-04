c05f1aedf3b95536b58683d4b55f96f309f8fb07
Jooyoung <brit7460@gmail.com>
2018-05-04 15:04:53 +0900

    fourth commit

--- app.py
+++ app.py
@@ -5,44 +5,26 @@ app = Flask(__name__)
 @app.route('/keyboard')
 def keyboard():
 	dataSend = {
-        "type" : "buttons",
-        "buttons" : ["시작하기", "도움말"]
-    }
+	"type" : "buttons",
+		"buttons" : ["시작하기", "도움말"]
+	}
  
-    return jsonify(dataSend)
+	return jsonify(dataSend)
 
 	
 @app.route('/message', methods=['POST'])
 def Message():
-    dataReceive = request.get_json()
-    content = dataReceive['content']
+	dataReceive = request.get_json()
+	content = dataReceive['content']
  
-    if content == u"시작하기":
-        dataSend = {
-            "message": {
-                "text": "아직 개발중이라 대답을 잘 못해도 이해해줘^^;"
-            }
-        }
-    elif content == u"도움말":
-        dataSend = {
-            "message": {
-                "text": "이제 곧 정식 버전이 출시될거야. 조금만 기다려~~~"
-            }
-        }
-    elif u"안녕" in content:
-        dataSend = {
-            "message": {
-                "text": "안녕~~ 반가워 ㅎㅎ"
-            }
-        }
-    else:
-        dataSend = {
-            "message": {
-                "text": "나랑 놀자 ㅋㅋㅋ"
-            }
-        }
  
-    return jsonify(dataSend)
+	if content == u"시작하기":
+		dataSend = {
+			"message": {
+				"text": "안녕"
+			}
+		}
+	return jsonify(dataSend)
 	
 if __name__ == '__main__':
 	app.run()
