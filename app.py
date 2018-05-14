be0c94ec059693803e6f22a50c2a2e48106281d4
Jooyoung <brit7460@gmail.com>
2018-05-14 14:02:00 +0900

    added DB boilerplate

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import urllib3
 import json
 from flask import Flask, request, jsonify
 from pymysql.cursors import DictCursor
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/keyboard')
 def keyboard():
