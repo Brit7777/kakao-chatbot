ab831469c882aaf12e197217d0e70b0117b5b55d
Jooyoung <brit7460@gmail.com>
2018-05-18 17:26:32 +0900

    commit1

--- app.py
+++ app.py
@@ -16,10 +16,15 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
 	location = db.Column(db.String(120), unique=True)
-
-	def __init__(self, name, location):
+	weather = db.Column(db.String(20))
+	#한식, 일식, 중식, 양식
+	type = db.Column(db.String(20))
+	
+	def __init__(self, name, location, weather, type):
 		self.name = name
 		self.location = location
+		self.weather = weather
+		self.type = type
 
 	def __repr__(self):
 		return '<Name %r>' % self.name
@@ -36,6 +41,18 @@ def keyboard():
 	
 @app.route('/message', methods=['POST'])
 def Message():
+	#초기 데이터 insert
+	insert_menulist('최우영스시', '서울 구로구 디지털로 288', '맑음')
+	insert_menulist('낭만부대찌개', '구로동 212-8 대륭포스트타워1차 B104호', '흐림')
+	insert_menulist('호우양꼬치', '서울 구로구 디지털로32나길 17-6', '비')
+	insert_menulist('멘무샤', '구로동 212-8 대륭포스트타워1차', '맑음')
+	insert_menulist('봉추찜닭', '구로동 188-25 지밸리비즈플라자', '맑음')
+	insert_menulist('포36거리', '구로동 212-8 대륭포스트타워1차', '비')
+	insert_menulist('5 pane', '서울 구로구 디지털로26길 111', '맑음')
+	insert_menulist('홍콩반점', '서울 구로구 구로동 1125-15', '흐림')
+	insert_menulist('coro', '서울 구로구 디지털로32다길 30', '흐림')
+	insert_menulist('영호돈까스', '서울 구로구 시흥대로163길 21', '맑음')
+	
 	dataReceive = request.get_json()
 	content = dataReceive['content']
  
@@ -56,7 +73,7 @@ def Message():
 		if string == "안녕":
 			dataSend = {
 				"message": {
-					"text": "안녕하세요~반갑습니다!"
+					"text": "안녕하세요~저도 반갑습니다:)"
 				}
 			}
 		elif string == "날씨":
@@ -73,6 +90,7 @@ def Message():
 					"text": "오늘의 점심은 " + str(menu) + "어때요?"
 				}
 			}
+		
 	return jsonify(dataSend)
 
 
@@ -88,11 +106,16 @@ def get_weather():
 	
 	return summary.group(1), nowTemp.group(1)
 
-def get_menu():
-	all_users = User.query.all()
-	db.session.delete(all_users)
+	
+def insert_menulist(name,location,weather,type): 
+	user = User(name,location,weather,type)
+	db.session.add(user)
 	db.session.commit()
-	return user.name
+
+def get_menu():
+	real_weather, temp = get_weather()
+	menu = User.query.filter_by(weather=real_weather).first()
+	return menu.name
 
 
 def get_text(text):
