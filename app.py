e2c411cdb22eb446ae461e20dbfb06b21362a5f0
Jooyoung <brit7460@gmail.com>
2018-05-21 11:01:59 +0900

    'commit2'

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class FoodList(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
