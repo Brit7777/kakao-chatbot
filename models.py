54c429b16146fab12a82fde93687140c64b68dfc
Jooyoung <brit7460@gmail.com>
2018-05-21 10:50:59 +0900

    'commit'

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-import app
+from app import app
 db = SQLAlchemy(app)
 
 class FoodList(db.Model):
+	__tablename__ = 'food_list'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
 	location = db.Column(db.String(120), unique=True)
