1d8bab6c32e2cda3bb8bb82a2ae944e9e0769145
Jooyoung Lee <35142536+Brit7777@users.noreply.github.com>
2018-05-30 11:44:30 +0900

    Update setInterval.js

--- setInterval.js
+++ setInterval.js
@@ -1,4 +1,15 @@
-var http = require("http");
-setInterval(function() {
-    http.get("http://mighty-spire-71391.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
+var requestLoop = setInterval(function(){
+  request({
+      url: "https://mighty-spire-71391.herokuapp.com",
+      method: "GET",
+      timeout: 10000,
+      followRedirect: true,
+      maxRedirects: 10
+  },function(error, response, body){
+      if(!error && response.statusCode == 200){
+          console.log('sucess!');
+      }else{
+          console.log('error' + response.statusCode);
+      }
+  });
+}, 60000);
